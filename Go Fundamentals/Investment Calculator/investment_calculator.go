9ea87dc7ac21b0aa8fc72a2b4e974584fc7e0473
leofs14 <leofariatech@gmail.com>
2025-11-14 22:18:40 +0000

    Situação 14/11

--- Go Fundamentals/Investment Calculator/investment_calculator.go
+++ Go Fundamentals/Investment Calculator/investment_calculator.go
@@ -5,23 +5,54 @@ import (
 	"math"
 )
 
-func main() {
+	// Criando variáveis globais
 	const inflationRate = 2.5
+
+func main() { //função obrigatória
+
+	// Variáveis
 	var investmentAmount, years float64 
 	expectedReturnRate := 5.5
 
-	fmt.Print("Investment Amount: ")
+	//Entrada de Dados:
+	// fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount) // & é um pointer.
 
-	fmt.Print("Expected Return Rate: ")
+	// fmt.Print("Expected Return Rate: ")
+	outputText("Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Years: ")
+	// fmt.Print("Years: ")
+	outputText("Years: ")
 	fmt.Scan(&years) 
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	inflationAdjustedValue := futureValue / math.Pow(1+inflationRate/100, years)
+	//Contas:
+	futureValue, inflationAdjustedValue := calculateFutureValue(investmentAmount, expectedReturnRate, years)
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	// inflationAdjustedValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(inflationAdjustedValue)
+	// Outputs:
+	formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
+	formattedIAV := fmt.Sprintf("Future Value(Adjusted To Inflation): %.1f\n", inflationAdjustedValue)
+	//fmt.Println("Future Value: ", futureValue)
+	//fmt.Printf("Future Value: %.1f\nFuture Value(Adjusted To Inflation): %.1f", futureValue, inflationAdjustedValue)
+	// fmt.Printf(`Future Value: %.1f
+
+	// Future Value(Adjusted To Inflation): %.1f`, futureValue, inflationAdjustedValue) // a crase pula a linha do texto
+	//fmt.Println("Future Value(Adjusted To Inflation): ", inflationAdjustedValue)
+	fmt.Print(formattedFV, formattedIAV)
+}
+
+//Função de Texto para Output
+func outputText(text string){
+	fmt.Print(text)
+}
+
+//Função para executar os cálculos
+func calculateFutureValue(investmentAmount, expectedReturnRate, years float64) (fv float64, iav float64) {
+	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	iav = fv / math.Pow(1+inflationRate/100, years)
+	// return fv, iav
+	return //outro jeito de retornar variáveis, mas quando tem mais variaveis é melhor usar o outro 
 }
